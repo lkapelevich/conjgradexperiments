73f976e39b82cdc5d98bb094f33ce93534b67bfa
Lea Kapelevich <leakapelevich@gmail.com>
2021-12-06 10:59:32 -0500

    refactor d

--- test_cgs.jl
+++ test_cgs.jl
@@ -9,23 +9,31 @@ include("conjutilities.jl")
 # include("grad_hess.jl")
 Random.seed!(1)
 num_samples = 1
-cone_d = 50
+cone_ds = [5, 25, 50]
 offsets = 10.0 .^ collect(-5:-1)
 
 ##
 abstract type Cone end
-struct SumLog <: Cone end
+struct SumLog <: Cone
+    d::Int
+end
 struct HypoPower <: Cone
     α::Vector{Float64}
 end
-HypoPower() = (α = rand(cone_d); α /= sum(α); HypoPower(α))
-struct HypoGeom <: Cone end
+HypoPower(d::Int) = (α = rand(d); α /= sum(α); HypoPower(α))
+struct HypoGeom <: Cone
+    d::Int
+end
 struct RadialPower <: Cone
     α::Vector{Float64}
 end
-RadialPower() = (α = rand(cone_d); α /= sum(α); RadialPower(α))
-struct RadialGeom <: Cone end
-struct InfinityNorm <: Cone end
+RadialPower(d::Int) = (α = rand(d); α /= sum(α); RadialPower(α))
+struct RadialGeom <: Cone
+    d::Int
+end
+struct InfinityNorm <: Cone
+    d::Int
+end
 
 ##
 function barrier(::SumLog)
@@ -44,10 +52,10 @@ function barrier(cone::HypoPower)
     end
     return bar
 end
-function barrier(::HypoGeom)
+function barrier(cone::HypoGeom)
     function bar(uw)
         (u, w) = (uw[1], uw[2:end])
-        return -log(exp(sum(log(wi) / cone_d for wi in w)) - u) - sum(log, w)
+        return -log(exp(sum(log(wi) / cone.d for wi in w)) - u) - sum(log, w)
     end
     return bar
 end
@@ -60,55 +68,58 @@ function barrier(cone::RadialPower)
     end
     return bar
 end
-function barrier(::RadialGeom)
+function barrier(cone::RadialGeom)
     function bar(uw)
         (u, w) = (uw[1], uw[2:end])
-        return -log(exp(sum(2 / cone_d * log(wi) for wi in w)) - u^2) -
-            sum((cone_d - 1) / cone_d * log(wi) for wi in w)
+        d = cone.d
+        return -log(exp(sum(2 / d * log(wi) for wi in w)) - u^2) - (d - 1) / d *
+            sum(log(wi) for wi in w)
     end
     return bar
 end
-function barrier(::InfinityNorm)
+function barrier(cone::InfinityNorm)
     function bar(uw)
         (u, w) = (uw[1], uw[2:end])
-        return sum(-log(u^2 - wi^2) for wi in w) + (cone_d - 1) * log(u)
+        return sum(-log(u^2 - wi^2) for wi in w) + (cone.d - 1) * log(u)
     end
     return bar
 end
 
-init_guess(::SumLog) = vcat(-1, 1, ones(cone_d))
-init_guess(::Union{HypoPower, HypoGeom}) = vcat(-1, ones(cone_d))
+init_guess(cone::SumLog) = vcat(-1, 1, ones(cone.d))
+init_guess(cone::HypoPower) = vcat(-1, ones(length(cone.α)))
+init_guess(cone::HypoGeom) = vcat(-1, ones(cone.d))
 init_guess(cone::RadialPower) = vcat(0, sqrt.(1 .+ cone.α))
-init_guess(::RadialGeom) = vcat(0, fill(sqrt.(1 + inv(cone_d)), cone_d))
-init_guess(::InfinityNorm) = vcat(1, zeros(cone_d))
+init_guess(cone::RadialGeom) = vcat(0, fill(sqrt.(1 + inv(cone.d)), cone.d))
+init_guess(cone::InfinityNorm) = vcat(1, zeros(cone.d))
 
-function dual_point(::SumLog, offset::Float64)
-    r = rand(cone_d)
+function dual_point(cone::SumLog, offset::Float64)
+    d = cone.d
+    r = rand(d)
     p = -rand()
-    phi = (p * sum(log(-ri / p) for ri in r) + p * cone_d)
+    phi = (p * sum(log(-ri / p) for ri in r) + p * d)
     q = phi * (1 + sign(phi) * offset)
     return vcat(p, q, r)
 end
 function dual_point(cone::HypoPower, offset::Float64)
     α = cone.α
-    r = rand(cone_d)
+    r = rand(length(α))
     phi = -exp(sum(αi * log(ri / αi) for (ri, αi) in zip(r, α)))
     p = phi * (1 - offset)
     return vcat(p, r)
 end
-dual_point(::HypoGeom, offset::Float64) = dual_point(HypoPower(), offset)
+dual_point(cone::HypoGeom, offset::Float64) = dual_point(HypoPower(cone.d), offset)
 function dual_point(cone::RadialPower, offset::Float64)
     α = cone.α
-    r = rand(cone_d)
+    r = rand(length(α))
     # only look at the p > 0 case, but that's OK because residuals aren't
     # going to behave differently in the other direction
     phi = exp(sum(αi * log(ri / αi) for (ri, αi) in zip(r, α)))
     p = phi * (1 - offset)
     return vcat(p, r)
 end
-dual_point(::RadialGeom, offset::Float64) = dual_point(RadialPower(), offset)
-function dual_point(::InfinityNorm, offset::Float64)
-    r = randn(cone_d)
+dual_point(cone::RadialGeom, offset::Float64) = dual_point(RadialPower(cone.d), offset)
+function dual_point(cone::InfinityNorm, offset::Float64)
+    r = randn(cone.d)
     p = sum(abs, r) * (1 + offset)
     return vcat(p, r)
 end
@@ -120,14 +131,15 @@ function grad_hess(cone::Cone, s)
     return ForwardDiff.hessian!(result, bar, s);
 end
 
-function dual_grad(::SumLog, pqr)
+function dual_grad(cone::SumLog, pqr)
+    d = cone.d
     (p, q, r) = (pqr[1], pqr[2], pqr[3:end])
     dual_ϕ = sum(log(r_i / -p) for r_i in r)
-    β = 1 + cone_d - q / p + dual_ϕ
-    bomega = cone_d * omegawright(β / cone_d - log(cone_d))
-    @assert bomega + cone_d * log(bomega) ≈ β
+    β = 1 + d - q / p + dual_ϕ
+    bomega = d * omegawright(β / d - log(d))
+    @assert bomega + d * log(bomega) ≈ β
 
-    cgp = (-cone_d - 2 + q / p + 2 * bomega) / (p * (1 - bomega))
+    cgp = (-d - 2 + q / p + 2 * bomega) / (p * (1 - bomega))
     cgq = -1 / (p * (1 - bomega))
     cgr = bomega ./ r / (1 - bomega)
     return (vcat(cgp, cgq, cgr), 0)
@@ -135,12 +147,13 @@ end
 function dual_grad(cone::HypoPower, pr)
     (p, r) = (pr[1], pr[2:end])
     α = cone.α
+    d = length(α)
     sumlog = sum(α_i * log(r_i) for (α_i, r_i) in zip(α, r))
 
-    h(y) = sum(ai * log(y - p * ai) for αi in α) - sumlog
-    hp(y) = sum(ai / (y - p * ai) for αi in α)
+    h(y) = sum(αi * log(y - p * αi) for αi in α) - sumlog
+    hp(y) = sum(αi / (y - p * αi) for αi in α)
     lower = 0.0
-    upper = exp(sumlog) + p / cone_d
+    upper = exp(sumlog) + p / d
     (new_bound, iter) = rootnewton(h, hp, lower = lower, upper = upper)
 
     dual_g_ϕ = inv(new_bound)
@@ -149,15 +162,17 @@ function dual_grad(cone::HypoPower, pr)
     return (vcat(cgp, cgr), iter)
 end
 function dual_grad(cone::HypoGeom, pr)
+    d = cone.d
     (p, r) = (pr[1], pr[2:end])
-    dual_ϕ = exp(sum(log, r) / cone_d)
-    cgp = -inv(p) - cone_d / (cone_d * dual_ϕ + p)
-    cgr = -1 / (1 + p / cone_d / dual_ϕ) ./ r
+    dual_ϕ = exp(sum(log, r) / d)
+    cgp = -inv(p) - d / (d * dual_ϕ + p)
+    cgr = -1 / (1 + p / d / dual_ϕ) ./ r
     return (vcat(cgp, cgr), 0)
 end
 function dual_grad(cone::RadialPower, pr)
     (p, r) = (pr[1], pr[2:end])
     α = cone.α
+    d = length(α)
     if iszero(p)
         cgp = 0
         cgr = -(α .+ 1) ./ r
@@ -165,40 +180,42 @@ function dual_grad(cone::RadialPower, pr)
         log_phi_r = 2 * sum(αi * log(ri) for (αi, ri) in zip(α, r))
         phi_r = exp(log_phi_r)
         phi_αr = exp(sum(αi * log(ri / αi) for (αi, ri) in zip(α, r)))
-        inner_bound = -1 / p - (1 + sign(p) * 1 / p * sqrt(phi_r * (cone_d^2 /
-            p^2 * phi_r + cone_d^2 - 1))) / (p / cone_d - cone_d * phi_r / p)
+        inner_bound = -1 / p - (1 + sign(p) * 1 / p * sqrt(phi_r * (d^2 / p^2 *
+            phi_r + d^2 - 1))) / (p / d - d * phi_r / p)
         gamma = abs(p) / phi_αr
-        outer_bound = (1 + cone_d) * gamma / (1 - gamma) / p
+        outer_bound = (1 + d) * gamma / (1 - gamma) / p
         h(y) = 2 * sum(αi * log(2 * αi * y^2 + (1 + αi) * 2 * y / p) for αi in α) -
             log_phi_r - log(2 * y / p + y^2) - 2 * log(2 * y / p)
         hp(y) = 2 * sum(αi^2 / (αi * y + (1 + αi) / p) for αi in α) -
             2 * (y + 1 / p) / y / (y + 2 / p)
         (cgp, iter) = rootnewton(h, hp, lower = inner_bound, upper = outer_bound,
-            init = inner_bound, increasing = false)
+            init = outer_bound, increasing = false)
         cgr = -(α * (1 + p * cgp) .+ 1) ./ r
     end
     return (vcat(cgp, cgr), iter)
 end
 function dual_grad(cone::RadialGeom, pr)
+    d = cone.d
     (p, r) = (pr[1], pr[2:end])
     if iszero(p)
         ctp = 0
-        cgr = -(inv(cone_d) .+ 1) ./ r
+        cgr = -(inv(d) + 1) ./ r
     else
-        phi_r = exp(2 * sum(log(ri) / cone_d for ri in r))
-        cgp = -1 / p - (1 + sign(p) * 1 / p * sqrt(phi_r * (cone_d^2 /
-            p^2 * phi_r + cone_d^2 - 1))) / (p / cone_d - cone_d * phi_r / p)
-        cgr = -((1 + p * cgp) / cone_d + 1) ./ r
+        phi_r = exp(2 * sum(log(ri) / d for ri in r))
+        cgp = -1 / p - (1 + sign(p) * 1 / p * sqrt(phi_r * ((d / p)^2 * phi_r +
+            d^2 - 1))) / (p / d - d * phi_r / p)
+        cgr = -((1 + p * cgp) / d + 1) ./ r
     end
     return (vcat(cgp, cgr), 0)
 end
 function dual_grad(cone::InfinityNorm, pr)
+    d = cone.d
     (p, r) = (pr[1], pr[2:end])
     dual_zeta = p - sum(abs, r)
     h(y) = p * y + sum(sqrt(1 + abs2(ri * y)) for ri in r) + 1
     hp(y) = p + y * sum(abs2(ri) / sqrt(1 + abs2(ri * y)) for ri in r)
-    # lower = -(cone_d + 1) / dual_zeta
-    upper = min(-inv(dual_zeta), -(cone_d + 1) / p)
+    # lower = -(d + 1) / dual_zeta
+    upper = min(-inv(dual_zeta), -(d + 1) / p)
     (cgp, iter) = rootnewton(h, hp, init = upper)
 
     cgr = copy(r)
@@ -260,42 +277,44 @@ cone_ts = [
     ]
 
 function get_resids()
-    results = DataFrame(
-        cone_d = Int[],
-        cone_type = Symbol[],
-        offset = Float64[],
-        trial = Int[],
-        residdirect = Float64[],
-        residnaive = Float64[],
-        newtoniters = Int[],
-        directiters = Int[],
-        )
+    for d in cone_ds
+        results = DataFrame(
+            cone_d = Int[],
+            cone_type = Symbol[],
+            offset = Float64[],
+            trial = Int[],
+            residdirect = Float64[],
+            residnaive = Float64[],
+            newtoniters = Int[],
+            directiters = Int[],
+            )
 
-    for C in cone_ts, o in offsets, i in 1:num_samples
-        cone = C()
-        z = dual_point(cone, o)
-        (g, iter) = dual_grad(cone, z)
-        resid = dot(z, g) + length(z)
-        # @show resid
-        (naive_g, naive_iter, resid_path) = naive_dual_grad(cone, z)
-        naive_resid = dot(z, naive_g) + length(z)
-        push!(results, (cone_d, nameof(C), o, i, resid,
-            naive_resid, naive_iter, iter))
-        if i == 1
-            resid_df = DataFrame(resid = resid_path)
-            resids_name = lowercase(string(nameof(C))) * string(Int(-log10(o)))
-            CSV.write(joinpath("csvs", resids_name * ".csv"), resid_df)
+        for C in cone_ts, o in offsets, i in 1:num_samples
+            cone = C(d)
+            z = dual_point(cone, o)
+            (g, iter) = dual_grad(cone, z)
+            resid = dot(z, g) + length(z)
+            # @show resid
+            (naive_g, naive_iter, resid_path) = naive_dual_grad(cone, z)
+            naive_resid = dot(z, naive_g) + length(z)
+            push!(results, (d, nameof(C), o, i, resid,
+                naive_resid, naive_iter, iter))
+            if i == 1
+                resid_df = DataFrame(resid = resid_path)
+                resids_name = lowercase(string(nameof(C))) * string(Int(-log10(o)))
+                CSV.write(joinpath("csvs", resids_name * ".csv"), resid_df)
+            end
         end
+
+        agg = combine(groupby(results, [:cone_d, :cone_type, :offset]),
+            :residdirect => mean => :residdirectmean,
+            :residnaive => mean => :residnaivemean,
+            :newtoniters => mean => :newtonitersmean,
+            :directiters => mean => :directitersmean,
+            )
+
+        CSV.write("csvs/cgs_$(d).csv", agg)
     end
-
-    agg = combine(groupby(results, [:cone_d, :cone_type, :offset]),
-        :residdirect => mean => :residdirectmean,
-        :residnaive => mean => :residnaivemean,
-        :newtoniters => mean => :newtonitersmean,
-        :directiters => mean => :directitersmean,
-        )
-
-    CSV.write("csvs/cgs.csv", agg)
     return
 end
 
